e3c47d02f926546aa4f8286a89cf84ec629794e3
Ji-arr <gfor508@gmail.com>
2024-02-22 01:28:11 +0800

    first commit

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,29 +1,28 @@
 import "./Project.css";
 export const Project = () => {
-  const sourcefile = `${process.env.PUBLIC_URL}`;
   const projects = [
     {
       id: 1,
       name: "Android App",
-      src: sourcefile + "mobile_app.jpg",
+      src: "mobile_app.jpg",
       link: "https://drive.google.com/drive/folders/1h-KtMV5hRb0q4X6D-C-W6cKUqu2YWSlh?fbclid=IwAR3ofhxdKKgt8RoJJZFe-05M5VnLRlUbvuKb8MfJp1BbgT0r93QVJtzaysQ",
     },
     {
       id: 2,
       name: "Library Management System",
-      src: sourcefile + "lms.jpg",
+      src: "lms.jpg",
       link: "https://drive.google.com/drive/folders/1pIVG2uNvPNKOf58vlURZyeKQ5gGzDCJa?fbclid=IwAR3ofhxdKKgt8RoJJZFe-05M5VnLRlUbvuKb8MfJp1BbgT0r93QVJtzaysQ",
     },
     {
       id: 3,
       name: "Programming",
-      src: sourcefile + "prog.jpg",
+      src: "prog.jpg",
       link: "https://github.com/Ji-arr",
     },
     {
       id: 4,
       name: "Application Programming Interface",
-      src: sourcefile + "api.jpg",
+      src: "api.jpg",
     },
   ];
   return (
@@ -37,7 +36,9 @@ export const Project = () => {
             <a href={project.link}>
               <div
                 className="project-card"
-                style={{ backgroundImage: `url(${project.src})` }}
+                style={{
+                  backgroundImage: `url(${process.env.PUBLIC_URL}/${project.src})`,
+                }}
               >
                 <div className="overlay">
                   <h1>{project.name}</h1>
